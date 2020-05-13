833cd278c45ac78aa1abd3c9a8a15d41eb49adff
Denis Tsarev <tsarev.work@mail.ru>
2020-05-13 12:37:39 +0300

    init dev

---
+++ src/ru/tsar/anagram/Anagram.java
@@ -0,0 +1,40 @@
+package ru.tsar.anagram;
+
+public class Anagram {
+
+	public String reverseText(String text) {
+		String[] words = text.split("\\s+");
+		StringBuilder result = new StringBuilder();
+
+		for (String word : words) {
+			result.append(reverseWord(word));
+			result.append(" ");
+		}
+		return result.toString();
+	}
+
+	private String reverseWord(String word) {
+		char[] chars = word.toCharArray();
+		int leftIndex = 0;
+		int rightIndex = word.length() - 1;
+
+		while (rightIndex > leftIndex) {
+			if (Character.isLetter(chars[leftIndex]) && Character.isLetter(chars[rightIndex])) {
+				swap(leftIndex, rightIndex, chars);
+				leftIndex++;
+				rightIndex--;
+			} else if (!Character.isLetter(chars[leftIndex])) {
+				leftIndex++;
+			} else if (!Character.isLetter(chars[rightIndex])) {
+				rightIndex--;
+			}
+		}
+		return (new String(chars));
+	} 
+
+	private void swap(int leftIndex, int rightIndex, char[] chars) {
+		char leftSymbol = chars[leftIndex];
+		chars[leftIndex] = chars[rightIndex];
+		chars[rightIndex] = leftSymbol;
+	}
+}
