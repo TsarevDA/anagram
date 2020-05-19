efe5010b70c3c6d79fbdf7efa80b1c0c3adc3e36
Denis Tsarev <tsarev.work@mail.ru>
2020-05-19 14:25:36 +0300

    JUnit test correction, pom.xml remove

--- test/ru/tsar/anagram/AnagramTest.java
+++ test/ru/tsar/anagram/AnagramTest.java
@@ -7,47 +7,66 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AnagramTest {
-	Anagram anagram;
+	
+private	Anagram anagram;
 	
 	@BeforeEach
 	void init (){
-		 anagram = new Anagram();
-		 
-	}
-	@Test
-	@DisplayName("Testing letters reverse") 
-	void testLetters() {
-		String expected = "dcba ";
-		String reality = anagram.reverseText("abcd");
-		assertEquals(expected,reality,"reverseText method should reverse input text");
-	}
-
-	@Test
-	@DisplayName("Testing non-letters staying") 
-	void testNonLetters() {		
-		String expected = "12!34$ ";
-		String reality = anagram.reverseText("12!34$");
-		assertEquals(expected,reality,"Non-letters symbols should stay on the same places");
+		 anagram = new Anagram(); 
 	}
 	
+	@Test
+	void givenNull_whenReverseText_thenNullPointerException() {
+		assertThrows(NullPointerException.class,()->anagram.reverseText(null));	
+	}
+	
+	@Test
+	void givenEmptyString_whenReverseText_thenSpace() {
+		String expected = "";
+		String actual = anagram.reverseText(" ");
+		assertEquals(expected,actual);
+	}
+		
+	@Test
+	void givenLettersWord_whenReverseText_thenReversedWord() {
+		String expected = "dcba ";
+		String actual = anagram.reverseText("abcd");
+		assertEquals(expected,actual);
+	}
 	
 	@Test
 	@DisplayName("Testing letters and non-letters") 
-	void allSymbols() {
+	void givenLettersAndNonLettersWord_whenReverseText_thenReversedWordNonLettersStayOnPlaces() {
 		String expected = "ab1234C;D ";
-		String reality = anagram.reverseText("DC1234b;a");
-		assertEquals(expected,reality,"letter symbols should be reversed and another "
-				+ "should stay on the same places");		
+		String actual = anagram.reverseText("DC1234b;a");
+		assertEquals(expected,actual);		
 	}
 	
 	@Test
-	@DisplayName("Testing several worlds") 
-	void testWords() {
-		String expected = "dcba dc11ba d#$7cba ";
-		String reality = anagram.reverseText("abcd ab11cd a#$7bcd");
-		assertEquals(expected,reality,"all words should be reverved separately, "
-				+ "Non-letters symbols should stay on the same places");
+	void givenNonLettersWord_whenReverseText_thenSameWord() {		
+		String expected = "12!34$ ";
+		String actual = anagram.reverseText("12!34$");
+		assertEquals(expected,actual);
+	}
+	
+	@Test	
+	void givenLettersText_whenReverseText_thenReversedWords() {
+			String expected = "dcba hgfe klj ";
+			String actual = anagram.reverseText("abcd efgh jlk");
+			assertEquals(expected,actual);
+	}
+	
+	@Test	
+	void givenLettersAndNonLettersText_whenReverseText_thenReversedWordsNonLettersStayOnPlaces() {
+			String expected = "dcba dc11ba d#$7cba ";
+			String actual = anagram.reverseText("abcd ab11cd a#$7bcd");
+			assertEquals(expected,actual);
 	}
 
-	
+	@Test	
+	void givenNonLettersText_whenReverseText_thenSameText() {
+			String expected = "123 4627 0213890 :2^7*2! ";
+			String actual = anagram.reverseText("123 4627 0213890 :2^7*2!");
+			assertEquals(expected,actual);
+	}	
 }
